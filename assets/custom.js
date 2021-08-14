c6c90d018b38926bee72b224af55b17d06887df9
Denise Oliveira <denise.ocrz@gmail.com>
2021-08-13 23:37:38 -0300

    Correções no código

--- assets/custom.js
+++ assets/custom.js
@@ -2,7 +2,7 @@ const botao = document.querySelector("#botaoCalcular")
 
 const calcularChurrasco = () => {
 
-    // Coleta valor dos inputs
+    // Coleta os valores dos inputs 
     const mulheres = document.getElementById('mulheres-qnt').value;
     const homens = document.getElementById('homens-qnt').value;
     const criancas = document.getElementById('criancas-qnt').value;
@@ -12,11 +12,11 @@ const calcularChurrasco = () => {
     const bebidasNaoAlcoolicas = document.getElementById('bebidas-nao-alcoolicas').value;
 
 
-    // Logica de valores por grama e multiplicacao da quantidade de pessoas
-    let totalCarne = mulheres * 150 + homens * 200 + criancas * 100
-
     // Soma da quantidade de pessoas
     const totalPessoas = Number(mulheres) + Number(homens) + Number(criancas)
+
+    // Logica de valores por grama e multiplicacao da quantidade de pessoas
+    let totalCarne = mulheres * 150 + homens * 200 + criancas * 100
     
     // 50g de acompanhamento por pessoa
     const totalAcompanhamento = acompanhamentos ? 50 * Number(totalPessoas) : 0
@@ -28,7 +28,6 @@ const calcularChurrasco = () => {
     const totalBebidasAlcoolicas = bebidasAlcoolicas ? 500 * Number(bebidasAlcoolicas) : 0
 
     
-
     document.getElementById("total-carne").innerHTML = `${getValueMeasure(totalCarne, 1)}`
     document.getElementById("total-pessoas").innerHTML = `${totalPessoas} pessoas`
     document.getElementById("total-acompanhamento").innerHTML = `${getValueMeasure(totalAcompanhamento, 1)}`
@@ -36,8 +35,6 @@ const calcularChurrasco = () => {
     document.getElementById("total-bebidas-alcoolicas").innerHTML = `${getValueMeasure(totalBebidasAlcoolicas, 2)}`
 
 }
-
-
 /* Definição da medida utilizada a partir da quantidade medida em gramas
 type 1: comidas
 type 2: bebidas
